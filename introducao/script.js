f605e5d56046a46fb1653c2f54a444298971bd12
Vitor Magalhaes <vitormagalhaes116@gmail.com>
2023-04-10 19:22:02 -0300

    add

---
+++ introducao/script.js
@@ -0,0 +1,40 @@
+
+// let texto = "Hello World";
+
+
+
+
+// console.log(
+//     document.getElementById("divJs").innerHTML
+// );
+
+// console.log(
+//     document.getElementById("divJs")
+// );
+
+// let largura = screen.width
+// let altura = screen.height
+// console.log(
+//     altura, largura
+// );
+
+// let frutas = document.getElementById("fruta").innerHTML;
+// let carros = document.getElementsByClassName("carro")[0].innerHTML;
+
+// console.log(
+//     carros
+// );
+
+
+// console.log(
+//     document.getElementsByTagName("div")
+// );
+
+
+
+
+// console.log(
+//     document.querySelectorAll(".carro")
+// );
+
+let nome = vitor;
---
+++ variaveis/script.js
@@ -0,0 +1,50 @@
+// console.log(
+//     document.querySelectorAll(".times")
+// );
+
+// console.log(
+//     document.getElementById("futebolEuropeu").querySelectorAll(".times")
+// )
+
+// let nome = [" vitor", " caio", " pablo"];
+
+// alert(nome)
+
+// let carro = ["fusca", "camaro", "gol"];
+
+// carro.forEach(carros => {
+//     console.log(carros)
+// });
+
+
+// let v1 = 34;
+// let v2 = 35;
+// const soma = v1 + v2;
+
+// console.log(soma)
+
+// nota1 = prompt("Qual a primeira nota? ");
+// nota2 = prompt("Qual a segunda nota? ");
+// media = (parseFloat(nota1) + parseFloat(nota2)) / 2;
+
+
+// if (media <img 5){
+//     alert("o aluno está com a média " +media+ " vermelha")
+// } else {
+//     alert("O aluno está com a média " +media+ " azul")
+// };
+
+// let nome = prompt("Qual o seu nome? ")
+// document.write("<h1>Bem vindo  ${nome} </h1>")
+// document.write("<img src='./idolo.jpg' style='width: 800px; height: 500px'> </img>")
+
+var numero1 = prompt("Qual o primeiro numero: ");
+var numero2 = prompt("Qual o segundo numero: ");
+
+var soma = numero1 + numero2;
+
+console.log(soma)
+
+
+
+
